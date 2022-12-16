df18e0a481728039b9ed59c71a6ba416484513d5
jarvis rangel <jarvijavier@gmail.com>
2022-12-16 17:43:13 -0400

    Agrega imagenes y header

---
+++ components/Header/Header.js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+export default function Header() {
+  return (
+    <>
+      <img src="/hero.png" id="logo" />
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
-
+import Layout from "../layaut/Layaut.js"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-     <h1 className='Caveat-Regular'> HOLA </h1>
-     <p className='a'>rr</p>
-    </>
+    <Layout className="home">
+        
+      </Layout>
   )
 }
